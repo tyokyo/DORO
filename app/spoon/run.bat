a39ac9533054a3cb7dc3a49a766d2b63d4b62838
qiang.zhang <qiang.zhang@ck-telecom.com>
2016-12-01 17:02:35 +0800

    elon

---
+++ app/spoon/run.bat
@@ -0,0 +1,2 @@
+java -jar spoon-runner-1.6.2-jar-with-dependencies.jar --title Iris4G  --apk ../../app/build/outputs/apk/app-debug.apk --test-apk ../../app/build/outputs/apk/app-debug-androidTest-unaligned.apk --grant-all --class-name iris4G.testcase.BurstCase
+
---
+++ app/spoon/runDebug1.bat
@@ -0,0 +1,2 @@
+java -jar spoon-runner-1.7.1-SNAPSHOT-jar-with-dependencies.jar  --apk ../../app/build/outputs/apk/app-debug.apk --test-apk ../../app/build/outputs/apk/app-debug-androidTest-unaligned.apk --class-name cn.testcase.me.FollowingCase  --method-name testPlayFollowingVideo --sequential
+
